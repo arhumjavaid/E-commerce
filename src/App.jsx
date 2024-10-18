b6158a8f31a185f62c7103dc4766db9d1254077d
Arhum Javaid <92026550+arhumjavaid@users.noreply.github.com>
2024-10-18 16:26:03 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Shop from "./pages/Shop"
+import ShopCategory from "./pages/ShopCategory"
+import Product from "./pages/Product"
+import Cart from "./pages/Cart"
+import LoginSignup from "./pages/LoginSignup"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Shop/>}/>
+        <Route path="/mens" element={<ShopCategory category="men"/>}/>
+        <Route path="/womens" element={<ShopCategory category="women"/>}/>
+        <Route path="/kids" element={<ShopCategory category="kid"/>}/>
+        <Route path="/product" element={<Product/>}>
+          <Route path=":productId" element={<Product/>}/>
+        </Route>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<LoginSignup/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import logo from "../assets/logo.png";
+import cart_icon from "../assets/cart_icon.png";
+import { useNavigate } from "react-router-dom";
+const Navbar = () => {
+  const navigate = useNavigate();
+  const [menu, setMenu] = useState("shop");
+  const nav = ["Shop", "Men", "Women", "Kids"];
+  const routes = ["/", "/mens", "/womens", "/kids"];
+
+  return (
+    <div className="flex justify-around p-6 shadow-[0_1px_3px_-2px_black]">
+      <div className="logo flex items-center gap-[10px]">
+        <img src={logo} alt="" />
+        <p className="text-[#171717] text-[38px] font-semibold">SHOPPER</p>
+      </div>
+      <ul className="flex items-center gap-12 list-none text-[#626262] text-xl font-medium">
+        {nav.map((item, index) => {
+          return (
+            <li
+              onClick={() => {
+                setMenu(item);
+                navigate(routes[index]);
+              }}
+              key={index}
+              className="flex flex-col items-center justify-center cursor-pointer gap-1 "
+            >
+              {item}
+              {menu === nav[index] ? (
+                <hr className="border-none w-[80%] h-[3px] rounded-xl bg-[#FF4141]" />
+              ) : (
+                <></>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+      <div className="flex items-center gap-11">
+        <button onClick={() => navigate("/login")} className="w-36 h-12 outline-none border-[1px] border-[#7a7a7a] rounded-[75px] text-[#515151] text-xl font-medium bg-white cursor-pointer active:bg-[#f3f3f3] active:scale-95">
+          Log In
+        </button>
+        <img onClick={() => navigate("/cart")} src={cart_icon} alt="" />
+        <div className="count w-6 h-6 flex items-center justify-center mt-[-35px] ml-[-55px] rounded-xl text-sm bg-red-700 text-white">
+          0
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
